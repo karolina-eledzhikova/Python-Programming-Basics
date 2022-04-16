07d103eaf24a18d226bb2f3cb3c181a7dfab4fc2
karolina-eledzhikova <103732237+karolina-eledzhikova@users.noreply.github.com>
2022-04-16 19:08:53 +0300

    Add files via upload

---
+++ for_ loop_exercise/06_oscars.py
@@ -0,0 +1,32 @@
+actor_name = input()
+total_points = float(input())
+number_of_jury = int(input())
+is_nominated = False
+for current_grade in range(number_of_jury):
+    current_name = input()
+    current_points = float(input())
+    current_final_points = len(current_name) * current_points / 2
+    total_points += current_final_points
+    if total_points > 1250.5:
+        is_nominated = True
+        break
+if is_nominated:
+    print(f"Congratulations, {actor_name} got a nominee for leading role with {total_points:.1f}!")
+else:
+    difference = 1250.5 - total_points
+    print(f"Sorry, {actor_name} you need {difference:.1f} more!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
